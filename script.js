3f5e8c645170fe476a2ad5587348c8e10d8a3db8
hrik-das <emptynull01@gmail.com>
2025-03-07 13:34:30 +0530

    minor bug fix

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-AOS.init();
-
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const headerMenu = document.querySelector("header ul");
+const menuButton = document.querySelector(".menu-btn");
+const headerMenuItems = document.querySelectorAll("li a");
+
+menuButton.addEventListener("click", function() {
+    headerMenu.classList.toggle("show");
+});
+
+headerMenuItems.forEach(function(item) {
+    item.addEventListener("click", function() {
+        headerMenu.classList.remove("show");
+    });
 });
